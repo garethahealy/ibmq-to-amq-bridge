cf98abc20778e68adc11175a740253bb624b3210
Gareth Healy <garethahealy@gmail.com>
2018-08-27 21:09:27 +0100

    Update install-mq-jars.sh

--- .travis/install-mq-jars.sh
+++ .travis/install-mq-jars.sh
@@ -2,10 +2,10 @@
 
 # grab the needed proprietary libs from the MQ container and install locally
 
-docker pull ibmcom/mq:8
+docker pull ibmcom/mq:9
 docker-compose -f .travis/docker-compose.yml up -d
 
-docker cp local-mq8:/opt/mqm/java/lib/com.ibm.mq.allclient.jar .
-docker cp local-mq8:/opt/mqm/java/lib/com.ibm.mq.pcf.jar .
+docker cp local-mq9:/opt/mqm/java/lib/com.ibm.mq.allclient.jar .
+docker cp local-mq9:/opt/mqm/java/lib/com.ibm.mq.pcf.jar .
 
-mvn install:install-file -Dfile=com.ibm.mq.allclient.jar -DgroupId=com.ibm.mq -DartifactId=allclient -Dversion=8.0.0.6 -Dpackaging=jar -B
+mvn install:install-file -Dfile=com.ibm.mq.allclient.jar -DgroupId=com.ibm.mq -DartifactId=allclient -Dversion=9.0.0 -Dpackaging=jar -B
